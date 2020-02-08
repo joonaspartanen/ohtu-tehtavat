36c2b56971348a5bdf5b515da36ea30c2301bee9
Joonas Partanen <partanen.joonas@gmail.com>
2020-02-08 16:39:37 +0200

    Submit exercise 3.1

---
+++ vk3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,25 @@
+
+package ohtu;
+
+public class Player {
+
+    private String name;
+    private String team;
+    private int goals;
+    private int assists;
+    private String nationality;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | team: " + team + " | goals: " + goals + " | assists: " + assists;
+    }
+
+}
